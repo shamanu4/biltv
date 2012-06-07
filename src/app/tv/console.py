ef980b9e29e855114fb4b0e84f83027df03dfb89
Your Name <shamanu4@gmail.com>
2012-06-07 16:32:46 +0300

    automake registers

--- src/app/tv/console.py
+++ src/app/tv/console.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 def send_cards():
-    from tv.models import CardDigital
+    from app.tv.models import CardDigital
     cc = CardDigital.objects.all().order_by('id')
     for c in cc:
         c.send()
+
+def payments_automake():
+    from app.tv.models import PaymentAutoMake
+    autoregs = PaymentAutoMake.objects.all()
+    for autoreg in autoregs:
+        payments = autoreg.payments.filter(maked=False)
+        for payment in payments:
+            payment.make()
+
