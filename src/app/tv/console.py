f2db04b70889266a2b899ea7437b47f7b6496fa5
Your Name <shamanu4@gmail.com>
2012-06-08 14:19:29 +0300

    not maked fees check cron job added

--- src/app/tv/console.py
+++ src/app/tv/console.py
@@ -14,3 +14,18 @@ def payments_automake():
         for payment in payments:
             payment.make()
 
+def fees_check():
+    from app.tv.models import Fee, CardService
+    for fee in Fee.objects.filter(maked__exact=False,deleted__exact=False,rolled_by__exact=None):
+        print fee
+        if not fee.card or not fee.tp:
+            fee.make()
+        else:
+            try:
+                cs = CardService.objects.get(card=fee.card,tp=fee.tp)
+            except CardService.DoesNotExist:
+                pass
+            else:
+                cs.activate(activated=fee.timestamp.date())
+                fee.delete()
+
