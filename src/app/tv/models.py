1f498165b840f86ab398960f6a65cc755621e0d2
Your Name <shamanu4@gmail.com>
2012-07-05 14:26:24 +0300

    bugfix 'invalid syntax', ('/usr/local/etc/django/biltv/src/app/tv/models.py', 616, 36, '        if self.bonus and self.card\n')

--- src/app/tv/models.py
+++ src/app/tv/models.py
@@ -606,14 +606,14 @@ class Fee(models.Model):
         self.prev = self.bill.balance_get()
         if self.fee_type and not self.fee_type.allow_negative:
             if self.sum > 0 and ((self.bill.balance_get() - self.sum) < -1):
-                self.descr = "Not enough money (%s<%s)" % (self.bill.balance_get(),self.sum)
+                self.descr = "Not enough money (%s < %s)" % (self.bill.balance_get(),self.sum)
                 self.save()
                 return (False,"Not enougn money")
         self.bill.balance = self.bill.balance - self.sum
         self.maked=True
         self.save()
         self.bill.save(last_operation_date=self.timestamp)
-        if self.bonus and self.card
+        if self.bonus and self.card:
             print "fee promotion"
             self.card.promotion(self)
         return (True,self)
