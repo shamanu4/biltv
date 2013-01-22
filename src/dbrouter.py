34e606a9bdc77c254098f842b8cb35a4d224f883
Your Name <shamanu4@gmail.com>
2013-01-22 16:16:50 +0200

    dbrouter

--- src/dbrouter.py
+++ src/dbrouter.py
@@ -3,20 +3,20 @@
 class MainDBRouter(object):
     
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'abills':
-            return 'abills'
+#        if model._meta.app_label == 'abills':
+#            return 'abills'
         return 'default'
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'abills':
-            return 'abills'
+#        if model._meta.app_label == 'abills':
+#            return 'abills'
         return 'default'
 
     def allow_relation(self, obj1, obj2, **hints):
         return True
 
     def allow_syncdb(self, db, model):
-        if model._meta.app_label == 'abills':
-            return False
+#        if model._meta.app_label == 'abills':
+#            return False
         return True
 
