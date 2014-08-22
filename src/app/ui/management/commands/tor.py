76adacb0feeb9ac16d49a7a20cca5084c38ade29
= <shamanu4@gmail.com>
2014-08-22 17:08:43 +0300

    fix tor command

--- src/app/ui/management/commands/tor.py
+++ src/app/ui/management/commands/tor.py
@@ -11,14 +11,44 @@ from django.core.management.base import BaseCommand, CommandError
 
 from ui.tornado_main import application
 from django.conf import settings
-from tv.models import CardService
 
 
 class Command(BaseCommand):
+    args = '[port_number]'
+    help = 'Starts the Tornado application for message handling.'
+
+    def sig_handler(self, sig, frame):
+        """Catch signal and init callback"""
+        tornado.ioloop.IOLoop.instance().add_callback(self.shutdown)
+
+    def shutdown(self):
+        """Stop server and add callback to stop i/o loop"""
+        self.http_server.stop()
+
+        io_loop = tornado.ioloop.IOLoop.instance()
+        io_loop.add_timeout(time.time() + 2, io_loop.stop)
 
     def handle(self, *args, **options):
-        css = CardService.objects.filter(extra__isnull=False)
-        for cs in css:
-            for link in cs.abills_links.all():
-                link.sync()
+        if len(args) == 2:
+            try:
+                port = int(args[1])
+            except ValueError:
+                raise CommandError('Invalid port number specified')
+            try:
+                address = str(args[0])
+            except ValueError:
+                raise CommandError('Invalid port number specified')
+        else:
+            port = int(settings.TORNADO_PORT)
+            address = str(settings.TORNADO_IP)
 
+        self.http_server = tornado.httpserver.HTTPServer(application)
+        self.http_server.listen(port, address)
+
+        # Init signals handler
+        signal.signal(signal.SIGTERM, self.sig_handler)
+
+        # This will also catch KeyboardInterrupt exception
+        signal.signal(signal.SIGINT, self.sig_handler)
+
+        tornado.ioloop.IOLoop.instance().start()
