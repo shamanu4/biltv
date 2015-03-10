746f7d3beefab8efe27b2372ad13d3340c5aee85
Maxim Musayev <shamanu4@gmail.com>
2015-03-10 12:39:35 +0200

    admin template fix

--- src/settings.py
+++ src/settings.py
@@ -73,7 +73,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-# ADMIN_MEDIA_PREFIX = '/media/'
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '**************************************************'
