0d716cd9a7fdda82d91b69b39727a0797cc9aee2
Maxim Musayev <shamanu4@gmail.com>
2015-04-16 11:01:11 +0300

    fix error - abon.rpc:175 TypeError: can't compare datetime.date to str

---
+++ src/app/tv/management/commands/bin_export.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'maxim'
+
+from django.core.management.base import BaseCommand, CommandError
+from scrambler.scrambler import UserExport, ChannelExport
+from django.conf import settings
+import subprocess
+
+settings.DEBUG = False
+HOST = "192.168.33.152"
+USER = "maxim"
+REMOTE_DIR = "~/scr"
+LOCAL = "../export/*.bin"
+SCR_IP = "192.168.17.41"
+
+SCP_PATH = "{user}@{host}:{dir}".format(user=USER, host=HOST, dir=REMOTE_DIR)
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print "bin export begin"
+        settings.DEBUG = False
+        ChannelExport.export()
+        print "prog.bin ready"
+        UserExport.export()
+        print "user.bin ready"
+        subprocess.call(["scp", LOCAL, SCP_PATH])
+        print "export done"
